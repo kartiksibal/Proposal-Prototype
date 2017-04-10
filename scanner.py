087316d9a20e09c6102d340ee950fcbd92a16422
kartik sibal <kartiksibal@gmail.com>
2017-04-10 21:25:30 +0530

    Minor changes

--- scanner.py
+++ scanner.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 """
-This script scrapes package vulnerability info. from 
-different datasets. It scrapes, CVE ID, Vulnerabiltiy 
+This script scrapes package vulnerability info from 
+different datasets. It scrapes CVE ID, Vulnerabiltiy 
 Status & package name from different datasets and creates 
-three list 'cve_id', 'pkg_name' & 'vuln_status', with all 
+three lists 'cve_id', 'pkg_name' & 'vuln_status', with all 
 the scraped information
 """
 import bs4 as bs 
@@ -13,9 +13,9 @@ import sys
 
 """Py version check"""
 if sys.version_info[0] == 3:
-    from urllib.request import urlopen as ur
-else:
-    from urllib import urlopen as ur
+    from urllib.request import urlopen
+else: 
+	from urllib import urlopen 
 
 cve_id = []
 pkg_name = []
@@ -23,17 +23,16 @@ vuln_status = []
 links = []
 
 def ubuntu_data():
-	
-	"""Scrapes data from Ubuntu's 'Main' dataset"""
-	url = ur("https://people.canonical.com/~ubuntu-security/cve/main.html").read()
-	soup = bs.BeautifulSoup (url, "lxml")
 
+	"""Scrapes data from Ubuntu's 'Main' dataset"""
+	url = urlopen("https://people.canonical.com/~ubuntu-security/cve/main.html")
+	soup = bs.BeautifulSoup (url, "lxml")
+	
 	"""
 	Scrapes vulnerability status.
 	Ubuntu provides a general vulnerability 
 	status of a package across all it's releases. 
 	"""
-
 	for tag in soup.find_all('tr'):
 		
 		if re.match('<\w+\s\w+="(\w+)">', str(tag)):
@@ -53,18 +52,15 @@ def ubuntu_data():
 			pkg_name.append(pkg[0])
 	'''
 	""" Uncomment to print scraped data"""
-	for id in range(len(cve_id)):
+	for cve, pkg, vuln in zip(cve_id, pkg_name, vuln_status):
 		print ("\nCVE ID:{}\
 			   \nPackage Name:{}\
-			   \nStatus:{}".format(cve_id[id], 
-				   		 		  pkg_name[id],
-				                  vuln_status[id]))
+			   \nStatus:{}".format(cve, pkg, vuln))
 	'''
 
 def debian_data():
-	
 	"""Scrapes vulnerability data from Debian's dataset"""
-	parent_url = ur ("https://security-tracker.debian.org/tracker/").read()
+	parent_url = urlopen("https://security-tracker.debian.org/tracker/")
 	soup = bs.BeautifulSoup (parent_url, "lxml")
 
 	"""Extracts links of child datasets"""
@@ -76,19 +72,17 @@ def debian_data():
 			links.append (href)
 
 	for child_links in range (6):
-		
 		"""Extracts package info from all the child datasets"""
-		child_url = ur ("https://security-tracker.debian.org" + links [child_links + 2]).read()
+		child_url = urlopen("https://security-tracker.debian.org" + links[child_links + 2])
 		soup = bs.BeautifulSoup (child_url, "lxml")
 
 		for tag in soup.find_all ('a'):
-			
 			href = tag.get('href')
-			
+
 			if re.search('/tracker/CVE-(.+)', href):
 				id = re.findall ('(?<=/tracker/).*', href)
 				cve_id.append(id[0])
-			
+
 			if re.search('^/tracker/TEMP-+.*', href):
 				id = re.findall ('(?<=/tracker/).*', href)
 				cve_id.append(id[0])
@@ -96,29 +90,27 @@ def debian_data():
 			if re.search('/tracker/source-package/(.+)', href):
 				pkg = re.findall ('(?<=/tracker/source-package/).*', href)
 				pkg_name.append(pkg[0])
-			
+
 			"""if package name is empty, use the previous package name"""
 			if href == "/tracker/source-package/":
 				pkg_name.append(pkg)
 		
 		for tag in soup.find_all('td'):
-			
 			if "medium**" in tag or "medium" in tag or "low" in tag or "low**" in tag or "not yet assigned" in tag:
 				vuln_status.append (tag.text)
 
 			elif tag.find_all("span", {"class":"red"}) and tag.text == "high**" or tag.text == "high":
 				vuln_status.append (tag.text)
-		'''
-		"""Uncomment to view scraped data"""
-		for id in range(len(cve_id)):
-			print ("\nCVE ID:{}\
-			       \nPackage Name:{}\
-			       \nStatus:{}".format(cve_id[id], 
-				   		 			pkg_name[id],
-				                 	vuln_status[id]))
-		'''
+	'''
+		""" Uncomment to print scraped data"""
+	for cve, pkg, vuln in zip(cve_id, pkg_name, vuln_status):
+		print ("\nCVE ID:{}\
+			   \nPackage Name:{}\
+			   \nStatus:{}".format(cve, pkg, vuln))
+	'''
 
 if __name__ == '__main__':
 
 	ubuntu_data()
 	debian_data()
+
